1f877bc2d5b9b4a713fd5277a5ce91bbdb319a03
sonetto104 <peter_regan88@yahoo.com>
2023-04-18 09:51:51 +0000

    Add display exceptions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -50,6 +51,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -86,4 +88,17 @@ function displayStatus(data) {
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 
+}
+
+function displayException(data) {
+    let heading = `An Exception Occurred`
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error numbers: <strong>${data.error_number}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show()
 }
